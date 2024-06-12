528e9c7d8e16f741df1537d715c6b0dd047ccf39
minaripenguin <minaripenguin@users.noreply.github.com>
2024-06-12 09:03:41 +0800

    SystemUI: Introduce lockscreen media art filter [1/2]
    * Alternative to lockscreen media feature which Google deprecated
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    Media Art: Cleanup and fixes

--- packages/SystemUI/src/com/android/systemui/qs/QSImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSImpl.java
@@ -71,6 +71,8 @@ import com.android.systemui.util.Utils;
 
 import dalvik.annotation.optimization.NeverCompile;
 
+import com.android.systemui.util.MediaArtUtils;
+
 import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.function.Consumer;
@@ -207,6 +209,7 @@ public class QSImpl implements QS, CommandQueue.Callbacks, StatusBarStateControl
      */
     public void onComponentCreated(QSComponent qsComponent, @Nullable Bundle savedInstanceState) {
         mRootView = qsComponent.getRootView();
+        MediaArtUtils.getInstance(mRootView.getContext()).setQSImpl(this);
 
         mQSPanelController = qsComponent.getQSPanelController();
         mQuickQSPanelController = qsComponent.getQuickQSPanelController();
@@ -719,6 +722,11 @@ public class QSImpl implements QS, CommandQueue.Callbacks, StatusBarStateControl
             mQsMediaHost.setSquishFraction(mSquishinessFraction);
         }
         updateMediaPositions();
+        if (!fullyCollapsed) {
+            MediaArtUtils.getInstance(mRootView.getContext()).hideMediaArt();
+        } else {
+            MediaArtUtils.getInstance(mRootView.getContext()).updateMediaArtVisibility();   
+        }
     }
 
     private void setAlphaAnimationProgress(float progress) {
