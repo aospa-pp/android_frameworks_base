6df47dc77162ee3e7542fa370fe8439e7c20336f
songlongdaiduong <song.long.dai.duong@gmail.com>
2025-01-10 15:47:37 +0800

    Revert "base: Implement Lineage Health Service"
    This reverts commit c5c2702e1e0f5c05792cca29b28861d3c3646b2d.

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -291,7 +291,6 @@ import com.android.server.webkit.WebViewUpdateService;
 import com.android.server.wm.ActivityTaskManagerService;
 import com.android.server.wm.WindowManagerGlobalLock;
 import com.android.server.wm.WindowManagerService;
-import com.android.server.lineage.health.HealthInterfaceService;
 
 import dalvik.system.VMRuntime;
 import dalvik.system.PathClassLoader;
@@ -2698,10 +2697,6 @@ public final class SystemServer implements Dumpable {
 
             t.traceBegin("StartBackgroundInstallControlService");
             mSystemServiceManager.startService(BackgroundInstallControlService.class);
-            t.traceEnd();
-
-            t.traceBegin("StartHealthService");
-            mSystemServiceManager.startService(HealthInterfaceService.class);
             t.traceEnd();
 
             t.traceBegin("StartPocketService");
