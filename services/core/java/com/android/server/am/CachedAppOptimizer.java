875362194af7938bc5e652be5e1494c198a1d1bb
Tim Murray <timmurray@google.com>
2025-02-13 13:45:58 -0800

    CachedAppOptimizer: limit to cached apps only
    Sometimes we'll freeze apps that aren't cached. Don't compact those.
    
    Test: cached apps remain compacted, uncached apps don't
    Flag: EXEMPT bugfix
    Bug: 370798593
    Change-Id: I72592cec5b6f1f3b345473e3f709bad7dc040b73

--- services/core/java/com/android/server/am/CachedAppOptimizer.java
+++ services/core/java/com/android/server/am/CachedAppOptimizer.java
@@ -1855,7 +1855,10 @@ public final class CachedAppOptimizer {
     void onProcessFrozen(ProcessRecord frozenProc) {
         if (useCompaction()) {
             synchronized (mProcLock) {
-                compactApp(frozenProc, CompactProfile.FULL, CompactSource.APP, false);
+                // only full-compact if process is cached
+                if (frozenProc.mState.getSetAdj() >= mCompactThrottleMinOomAdj) {
+                    compactApp(frozenProc, CompactProfile.FULL, CompactSource.APP, false);
+                }
             }
         }
         frozenProc.onProcessFrozen();
